1eae0bd2aa36ea3159f91b368d8745f2f08bd9d7
Ryan Otto <rotto@drew.edu>
2017-10-27 23:12:19 -0400

    Add 5 more field scrapes
    * quantity, tags, materials, images and variant names are now included in HTML parsing

--- EtsyScraper.py
+++ EtsyScraper.py
@@ -3,36 +3,68 @@ import urllib.request
 import csv
 
 linkWord="word"
-url = 'https://www.etsy.com/listing/275754976/vintage-forest-green-red-silk-traveler'
+# url = 'https://www.etsy.com/listing/275754976/vintage-forest-green-red-silk-traveler'
+url = 'https://www.etsy.com/se-en/listing/483597760/christian-t-shirt-choose-joy-tshirt?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=&ref=sr_gallery_22'
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page.read(), "html.parser")
 
-#Field values
+# Field values
 title = soup.find("span", {"itemprop":"name"} ).text
 description = soup.find("div", {"id":"item-description"} ).text
 price = soup.find(itemprop="price").get("content")
 currencyCode = soup.find(itemprop="currency").get("content")
 
-quantity = soup.find("span", {"itemprop":"name"} ).text
-tags = soup.find("span", {"itemprop":"name"} ).text
-materials = soup.find("span", {"itemprop":"name"} ).text
-image1 = soup.find("span", {"itemprop":"name"} ).text
-image2 = soup.find("span", {"itemprop":"name"} ).text
-image3 = soup.find("span", {"itemprop":"name"} ).text
-image4 = soup.find("span", {"itemprop":"name"} ).text
-image5 = soup.find("span", {"itemprop":"name"} ).text
-variation1Type = soup.find("span", {"itemprop":"name"} ).text
-variation1Name = soup.find("span", {"itemprop":"name"} ).text
-variation1Values = soup.find("span", {"itemprop":"name"} ).text
-variation2Type = soup.find("span", {"itemprop":"name"} ).text
-variation2Name = soup.find("span", {"itemprop":"name"} ).text
-variation2Values = soup.find("span", {"itemprop":"name"} ).text
+# Quantity
+for selector in soup.find_all( "select", {"id":'inventory-select-quantity'} ):
+   for child in selector.find_all('option'):
+    quantity = child.string
+
+# Tags
+tags = []
+for selector in soup.find_all( "ul", {"id":'listing-tag-list'} ):
+   for child in selector.find_all('li'):
+    tags.append( child.text.replace("\n", "") )
+tags = str.join(', ', tags)
 
 
-print(title)
-print(description)
-print(price)
-print(currencyCode)
+materials = soup.find("span", {"id":"overview-materials"} ).text
+
+
+# Images
+images=[]
+for selector in soup.find_all("ul", {"id":"image-carousel"} ):
+    for child in selector.find_all('li'):
+        for childchild in selector.find_all('img'):
+            images.append(childchild.get("src"))
+images = str.join(", ", images)
+
+# Variant categories
+variantRawCategories = soup.find_all("label")
+variantFinalCategories = []
+for category in variantRawCategories:
+    if 'inventory-variation-select' in str((category.get('for'))):
+        variantFinalCategories.append(category.text)
+variantFinalCategories = str.join(", ", variantFinalCategories)
+print(variantFinalCategories)
+
+
+
+variationValues = soup.find_all("select", {"class": "variation-select"})
+
+# print(variantCategories)
+# for value in variationValues:
+#     print(value)
+
+
+
+# print(title)
+# print(description)
+# print(price)
+# print(currencyCode)
+# print(quantity)
+# print(tags)
+# print(materials)
+# print(images)
 
 
 # with open('sample.csv','w', newline='') as csvfile:
