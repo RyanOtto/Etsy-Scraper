b6105ad049d881e6ac776a9f8fcbd1a00bd7ce06
Ryan Otto <rotto@drew.edu>
2017-10-31 17:14:37 -0400

    Field fixes, CSV write modifications
    * Description should now be properly scraped
    * All fields have preliminary implementations now, and will be worked on as bugs are found
    * Preliminary CSV appending functionality implemented

--- EtsyScraper.py
+++ EtsyScraper.py
@@ -2,17 +2,17 @@ from bs4 import BeautifulSoup
 import urllib.request
 import csv
 
-linkWord="word"
-# url = 'https://www.etsy.com/listing/275754976/vintage-forest-green-red-silk-traveler'
 url = 'https://www.etsy.com/se-en/listing/483597760/christian-t-shirt-choose-joy-tshirt?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=&ref=sr_gallery_22'
+# url='https://www.etsy.com/se-en/listing/559679743/extra-detailed-paw-patrol-logo-multi?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=&ref=sc_gallery_1&plkey=1fa7bdf685bee3bba74bd3f5ac773da0cf96ffa8:559679743'
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page.read(), "html.parser")
 
 # Field values
 title = soup.find("span", {"itemprop":"name"} ).text
-description = soup.find("div", {"id":"item-description"} ).text
+description = soup.find("meta", {"name":"description"} ).get("content")
 price = soup.find(itemprop="price").get("content")
 currencyCode = soup.find(itemprop="currency").get("content")
+materials = soup.find("span", {"id":"overview-materials"} ).text
 
 # Quantity
 for selector in soup.find_all( "select", {"id":'inventory-select-quantity'} ):
@@ -26,17 +26,10 @@ for selector in soup.find_all( "ul", {"id":'listing-tag-list'} ):
     tags.append( child.text.replace("\n", "") )
 tags = str.join(', ', tags)
 
-
-materials = soup.find("span", {"id":"overview-materials"} ).text
-
-
 # Images
 images=[]
-for selector in soup.find_all("ul", {"id":"image-carousel"} ):
-    for child in selector.find_all('li'):
-        for childchild in selector.find_all('img'):
-            images.append(childchild.get("src"))
-images = str.join(", ", images)
+carouseEntries = soup.select("#image-carousel > li")
+for image in carouseEntries: images.append(image.get('data-full-image-href'))
 
 # Variant categories
 variantRawCategories = soup.find_all("label")
@@ -44,25 +37,29 @@ variantFinalCategories = []
 for category in variantRawCategories:
     if 'inventory-variation-select' in str((category.get('for'))):
         variantFinalCategories.append(category.text)
-variantFinalCategories = str.join(", ", variantFinalCategories)
-
 
 # Variant choices (or values)
 variationValuesRaw = soup.find_all("select", {"class": "variation-select"})
 variationValues = []
 currentList = []
-i=0
 for value in variationValuesRaw:
     valueList = value.text
     valueList = valueList.split('\n')
-    variationValues.append(valueList)
+    new_array = [x for x in valueList if str(x) != 'Select an option']
+    variationValues.append(new_array[1:len(valueList)-2])
 
-print(variationValues)
+# finalthing=[]
+# i =0
+# for x in variationValues:
+#     x = str.join(',', variationValues[i])
+#     i+=1
+#     finalthing.append(x)
+# print(finalthing)
 
 
 
 # print(title)
-# print(description)
+print(description)
 # print(price)
 # print(currencyCode)
 # print(quantity)
@@ -70,11 +67,15 @@ print(variationValues)
 # print(materials)
 # print(images)
 # print(variantFinalCategories)
+# print(variationValues)
 
+# Fields to be written
+# writeImages = ""
+# writeVariantFinalCategories = ""
+# write variationValues = ""
 
+# For every image, add "image N" to fields
 
-    # with open('sample.csv','w', newline='') as csvfile:
-#     writer = csv.writer(csvfile)
-#     writer.writerow(["Field 1", "Field 2", "Field 3", "Field 4", "Field 5", "Field 6", "Field 7"])
-#     writer.writerows([["a","b","c"],["a","b","c"],["a","b","c"],["a","b","c"],["a","b","c"],["a","b","c"],
-#                     ["a", "b", "c"],["a","b","c"],["a","b","c"],["a","b","c"],["a","b","c"]])
+with open('sample.csv','a', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows([[ title, description, price, currencyCode, quantity, tags, materials ]])
